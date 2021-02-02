3478e9b8bd459265ffc2715288bec40f61c220cd
onurbasdas <onurebasdase@hotmail.com>
2021-02-02 17:12:51 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Onur Başdaş on 2.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
