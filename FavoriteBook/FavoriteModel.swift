e9fbc2858322fa5532c29c5417f5f2af9ef0921e
onurbasdas <onurebasdase@hotmail.com>
2021-02-02 17:47:18 +0300

    add list view completed

--- FavoriteBook/FavoriteModel.swift
+++ FavoriteBook/FavoriteModel.swift
@@ -20,8 +20,19 @@ struct FavoriteElements : Identifiable {
     var description : String
 }
 
+
+//Characters
 let bart = FavoriteElements(name: "Bart", imagename: "bart", description: "The Simpson Bart")
 let homer = FavoriteElements(name: "Homer", imagename: "homer", description: "The Simpson Homer")
 let marge = FavoriteElements(name: "Marge", imagename: "marge", description: "The Simpson Marge")
 
-let favoriteCharacters = FavoriteModel(title: "Favorite Character", elements: [bart, homer, marge])
+let favoriteCharacters = FavoriteModel(title: "Favorite Character", elements: [bart,homer,marge])
+
+//Locations
+let collesium = FavoriteElements(name: "Collesium", imagename: "collesium", description: "The Location Collesium")
+let greatwall = FavoriteElements(name: "Great Wall", imagename: "greatwall", description: "The Location Great Wall")
+let kremlin = FavoriteElements(name: "Kremlin", imagename: "kremlin", description: "The Location Kremlin")
+
+let favoriteLocations = FavoriteModel(title: "Favorite Location", elements: [collesium,greatwall,kremlin])
+
+let myFavorites = [favoriteCharacters,favoriteLocations]
